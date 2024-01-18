924a3477f7db5db463b565b3e675c89ca529736a
ronishaiz <69716822+ronishaiz@users.noreply.github.com>
2024-01-18 18:19:00 +0200

    bigger font for tree emoji

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ from backend.get_url import get_todays_url
 st.set_page_config(
     page_title="Welcome",
     page_icon="👋",
+    layout="wide"
 )
 
 url = get_todays_url()
-st.write("# [🌳](%s)" % url)
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:300px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+html_code = f'<a class="big-font" href="{url}">🌳</a>'
+st.markdown(html_code, unsafe_allow_html=True)
 
 
 st.write("for Spark, with love. 💚")
